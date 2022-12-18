c43b86b764722f68b0e4f970123d73127b321234
Vasilev Kirill <kirya-vasilev-1994@mail.ru>
2022-12-18 05:37:23 +0300

    Добавил домашнюю работу по седьмому семинару

---
+++ work2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача №50. Программа, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+Console.WriteLine("Введите размерность строк");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите размерность столбцов");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        array[i, j] = Convert.ToInt32(new Random().Next(0, 21));
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+        Console.Write(array[i, j] + "\t  ");
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите координаты");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > m && b > n)
+    Console.WriteLine("Такого числа нет");
+else
+{
+    object c = array.GetValue(a, b);
+    Console.WriteLine(c);
+}
